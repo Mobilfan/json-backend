9ba1ab2705eacf6c67f4db0e5d26c64feccaf2f4
Mobilfan <43099419+Mobilfan@users.noreply.github.com>
2020-04-01 15:14:21 +0200

    Update jsonbackend.py

--- jsonbackend.py
+++ jsonbackend.py
@@ -5,45 +5,45 @@ def _access_daemon(filename="data.json"):
   cache = cache(10)
   #Customize this function to fit to the json structure used
   while True:
-  	if len(jsonQueue) != 0:
-    	instruction = jsonQueue[0]
-      logging.info(f'New instruction:\n{instruction}')
-      if instruction["action"] >= 3: raise RuntimeError
+		if len(jsonQueue) != 0:
+		instruction = jsonQueue[0]
+		logging.info(f'New instruction:\n{instruction}')
+    if instruction["action"] >= 3: raise RuntimeError
 
-      logging.debug(f'Loading JSON from {filename}')
-      data = json.load(open(filename))
-      logging.debug(f'JSON data:\n{data}')
+    logging.debug(f'Loading JSON from {filename}')
+    data = json.load(open(filename))
+    logging.debug(f'JSON data:\n{data}')
 
-      #0 is write
-      if instruction["action"] == 0:
-      	option, value, server = instruction["option"]
-        data[server][option] = value
-        json.dump(data,open(filename,'w+'))
+    #0 is write
+    if instruction["action"] == 0:
+     	option, value, server = instruction["option"]
+      data[server][option] = value
+      json.dump(data,open(filename,'w+'))
 
-     	#1 is read
-      elif instruction["action"] == 1:
-      	option, server = instruction["option"]
-        value = cache.get( [option, server] )
+    #1 is read
+    elif instruction["action"] == 1:
+    	option, server = instruction["option"]
+      value = cache.get( [option, server] )
                                 
-        if value == None:
-        	if option in data[server]:
-          	value = data[server][option]
-            cache.append( (option, server), value)
-            instruction["return"].append(value)
-          else: instruction["return"].append(None)
-        else: instruction["return"].append(value)
+      if value == None:
+      	if option in data[server]:
+        	value = data[server][option]
+         	cache.append( (option, server), value)
+          instruction["return"].append(value)
+    		else: instruction["return"].append(None)
+     	else: instruction["return"].append(value)
                                 
-        #2 is new server
-        elif instruction["action"] == 2:
-        	server = instruction["option"]
-          if not server in data:
-          	data[server] = {}
-            json.dump(data,open(filename,'w+'))
+     	#2 is new server
+   		elif instruction["action"] == 2:
+				server = instruction["option"]
+     		if not server in data:
+        	data[server] = {}
+        	json.dump(data,open(filename,'w+'))
 
-            del jsonQueue[0]
-            logging.debug(f'New queue: {jsonQueue}')
+         	del jsonQueue[0]
+         	logging.debug(f'New queue: {jsonQueue}')
 						
-          else: time.sleep(0.1)
+         	else: time.sleep(0.1)
 
 def _start():
   global jsonQueue
